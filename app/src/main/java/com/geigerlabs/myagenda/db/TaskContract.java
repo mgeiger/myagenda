51ce6019ef9ce2a22dbde6cea432ccbbcb2162f2
Matthew J. Geiger <matthew.j.geiger@gmail.com>
2016-10-05 20:37:32 -0400

    Initial Commit.

---
+++ app/src/main/java/com/geigerlabs/myagenda/db/TaskContract.java
@@ -0,0 +1,15 @@
+package com.geigerlabs.myagenda.db;
+
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+    public static final String DB_NAME = "com.geigerlabs.myagenda.db";
+    public static final int DB_VERSION = 2;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+        public static final String COL_TASK_TITLE = "title";
+        public static final String COL_TASK_DELETED = "deleted";
+    }
+}
